69f4ac43caaf2735b553809a19304364be9a209b
Miquel Arranz <miquel.arranz@typeform.com>
2025-10-04 13:10:13 +0200

    feat: add a scanner menu example

--- src/Modulo3/Main.java
+++ src/Modulo3/Main.java
@@ -1,5 +1,7 @@
 package Modulo3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -37,5 +39,45 @@ public class Main {
             }
             System.out.println();
         }
+
+        System.out.println("----- Menu Scanner ----");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
+
+        while (option != 5) {
+            System.out.println("\n=== Soccer Menu ===");
+            System.out.println("1. Show Player Info");
+            System.out.println("2. Show Team Info");
+            System.out.println("3. Player Action");
+            System.out.println("4. Team Action");
+            System.out.println("5. Exit");
+            System.out.print("Select an option: ");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Show Player Info action has been executed");
+                    break;
+                case 2:
+                    System.out.println("Show Team Info action has been executed");
+                    break;
+                case 3:
+                    System.out.println("Player Action has been executed");
+                    break;
+                case 4:
+                    System.out.println("Team Action has been executed");
+                    break;
+                case 5:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        scanner.close();
+
     }
 }
